bddf05ae02c69565e81508c0b559c689a09dc290
glendo <gleneggers64@gmail.com>
2021-05-13 07:58:10 -0500

    fix undeclared variable bug

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -70,6 +70,8 @@ router.get('/post/:id', (req, res) => {
                 return;
             }
 
+            const post = dbPostData.get({ plain: true });
+
             res.render('single-post', {
                 post,
                 loggedIn: req.session.loggedIn
